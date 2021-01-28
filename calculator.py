25d2abbf4919dff16f03db401e76bc21cd483a46
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-01-28 16:25:32 -0500

    adding CalculatorError

--- calculator.py
+++ calculator.py
@@ -5,7 +5,11 @@ class Calculator():
     """Calculator for trying how pytest works"""
 
     def add(self, a , b):
-        return a + b
+        try:
+            return a + b
+        except TypeError:
+            raise CalculatorError('this does not accept a sring value')
+
 
     def subtract(self, a, b ):
         return a - b
--- test_calculator.py
+++ test_calculator.py
@@ -5,8 +5,8 @@ import pytest
 
 def test_add():
     calculator = Calculator()
-    result = calculator.add(5,7)
-    assert 12==result
+    result = calculator.add("helo", "helo")
+    assert "helohelo" == result
 
 
 def test_substract():
@@ -20,8 +20,15 @@ def test_divide():
     result = calculator.divide(10,2)
     assert 5 == result
 
+
 def test_do_some_weird_stuff():
     calculator = Calculator()
+    with pytest.raises(CalculatorError) as context:
+        result = calculator.add("hello",2)
+    
+    assert str(context.value) == 'this does not accept a sring value'
+
+def test_do_some_weirder_stuff():
+    calculator = Calculator()
     with pytest.raises(CalculatorError):
-        result = calculator.subtract('add',2)
-    assert 5 == result
+        result = calculator.add("a","b")
